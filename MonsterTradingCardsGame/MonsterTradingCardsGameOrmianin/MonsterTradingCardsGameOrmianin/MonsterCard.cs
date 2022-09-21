a2dce3cf8956749f7e4092d169082b16d37ca135
TORz69 <91666614+TORz69@users.noreply.github.com>
2022-09-21 12:37:40 +0200

    Feat 1: Basic Structure

---
+++ MonsterTradingCardsGame/MonsterTradingCardsGameOrmianin/MonsterTradingCardsGameOrmianin/MonsterCard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardsGameOrmianin {
+    internal class MonsterCard : Card {
+        public MonsterCard(string name, int damage, char element) : base("Monster Card", 20, 'e') {
+            //ctor
+        }
+    }
+}
---
+++ MonsterTradingCardsGame/MonsterTradingCardsGameOrmianin/MonsterTradingCardsGameOrmianin/SpellCard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardsGameOrmianin {
+    internal class SpellCard : Card {
+        public SpellCard(string name, int damage, char element) : base("Spell Card", 50, 'a') {
+            //ctor
+        }
+    }
+}
