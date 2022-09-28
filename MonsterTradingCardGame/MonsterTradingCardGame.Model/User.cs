d023f705cedacfb746661af35a80dfdce36eeaa4
TORz69 <91666614+TORz69@users.noreply.github.com>
2022-09-28 11:41:05 +0200

    2: New Try

---
+++ MonsterTradingCardGame/MonsterTradingCardGame.Model/User.cs
@@ -0,0 +1,13 @@
+﻿namespace MonsterTradingCardGame.Model {
+    public class User {
+        //fields
+        string name = "";
+        string password = "";
+
+        //ctor
+        public User(string name, string password) {
+            this.name = name;
+            this.password = password;
+        }
+    }
+}
