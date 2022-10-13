a6177a7def2398cb3f8a8e67674f959605a52b01
TORz69 <91666614+TORz69@users.noreply.github.com>
2022-10-13 14:08:28 +0200

    3: Zwischenstopp

--- MonsterTradingCardGame/MonsterTradingCardGame.Model/User.cs
+++ MonsterTradingCardGame/MonsterTradingCardGame.Model/User.cs
@@ -1,13 +1,25 @@
 ﻿namespace MonsterTradingCardGame.Model {
     public class User {
-        //fields
-        string name = "";
-        string password = "";
-
-        //ctor
+        // ctor
         public User(string name, string password) {
             this.name = name;
             this.password = password;
+        }
+        
+        // fields
+        string name = "";
+        string password = "";
+        Deck deck = new Deck();
+
+        // properties
+        public string Name {
+            get => name;
+            set => name = value;
+        }
+        
+        public string Password {
+            get => password;
+            set => password = value;
         }
     }
 }
